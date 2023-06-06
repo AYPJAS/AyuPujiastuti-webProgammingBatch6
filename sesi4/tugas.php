aba6ff10f37db1e0611220415c643f23bac9b5b9
Nama_Register_Github <pujiayu0120@gmail.com>
2023-06-06 11:57:59 +0800

    update data

---
+++ sesi4/tugas.php
@@ -0,0 +1,21 @@
+<?php
+  echo "Kode Program PHP - Keliling dan Luas Lingkaran";
+  echo "<hr>";
+ 
+  $jari2 = 25;
+   
+  echo "Jari-jari lingkaran = $jari2<br>";
+ 
+  echo "<br>";
+ 
+  echo "Keliling lingkaran = ".round(22/7 * 2 * $jari2,2)."<br>";
+  echo "Luas lingkaran = ". round(22/7 * $jari2 * $jari2,2) ."<br>";
+?>
+
+- masukan nilai jari Jari
+- proses mengitung luas lingkaran
+- proses mengitung keliling lingkaran
+- tampilkan luas lingkaran
+- tampilkan keliling lingkaran
+
+
